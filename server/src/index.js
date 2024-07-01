15ad3cdcbb206a77e63c485b4e22585752a8821e
shanuka dilshan <shanndilshanx@gmail.com>
2024-07-01 17:06:16 +0530

    7-1-1

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+//Setup middlewears 
+app.use(express.json());
+app.use(cors());
+
+
+//listen to a port
+app.listen(5000 , () => {
+    console.log("SERVER STARTED")
+});
